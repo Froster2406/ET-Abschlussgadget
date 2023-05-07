c696e1dcbda024ff55e8dd0d472079db2bf23825
Felix Reeee <94645228+FelixReeee@users.noreply.github.com>
2023-05-07 14:36:33 +0200

    Add serial debug output

--- Software/ATTINY414_Firmware/ATTINY414_Firmware/main.c
+++ Software/ATTINY414_Firmware/ATTINY414_Firmware/main.c
@@ -1,10 +1,10 @@
 // 10MHz
 #define F_CPU (1000000)
-#define VALUE1 (300)
-#define VALUE2 (400)
-#define VALUE3 (500)
-#define VALUE4 (650)
-#define VALUE5 (800)
+#define VALUE1 (1300)
+#define VALUE2 (1400)
+#define VALUE3 (1500)
+#define VALUE4 (1650)
+#define VALUE5 (1800)
 
 #include <avr/io.h>
 #include <util/delay.h>
@@ -12,7 +12,7 @@
 #include "TinyTouchLib.h"
 
 uint8_t irCmd[] = {0x76, 0xB5, 0xAA, 0xB5, 0xAD};
-int16_t bias;
+uint16_t bias;
 
 void pcbTest(void)
 {		
@@ -48,37 +48,76 @@ void pcbTest(void)
 }
 
 // output data on PB0 (J1)
-void printADCValue(uint8_t value)
+void printChar(uint8_t value)
 {
-	// configure pin as output
+	// Configure pin as output
 	PORTB.DIRSET = 0b1;
+	
+	// Start bit
+	PORTB.OUTCLR = 0b1;
+	_delay_us(319);
+	
 	for(uint8_t i = 0; i < 8; i++){
 		if((value >> i) & 1) {
-			// toggle pin ON / HIGH
+			// Toggle pin ON / HIGH
 			PORTB.OUTSET = 0b1;
 		}
 		else {
-			// toggle pin OFF / LOW
+			// Toggle pin OFF / LOW
 			PORTB.OUTCLR = 0b1;
 		}
-		_delay_ms(1);
+		_delay_us(319);
 	}
+	
+	// Stop bit
+	PORTB.OUTSET = 0b1;
+	_delay_us(319);
+	
+	// Idle
+	PORTB.OUTSET = 0b1;
+}
+
+void serialPrint(uint16_t val) {
+	char buf[8 * sizeof(uint16_t) + 1];
+	char *str = &buf[sizeof(buf) - 1];
+
+	*str = '\0';
+	
+	if (val < 0) {
+		printChar('-');
+	}
+	
+	uint8_t len = 0;
+	do {
+		char c = val % 10;
+		val /= 10;
+
+		*--str = c + '0';
+		len++;
+	} while(val);
+	
+	for (uint8_t i = 0; i < len; i++) {
+			printChar(str[i]);
+	}
+	printChar('\n');
 }
 
 void actualProgram(void)
 {
 	for(;;)
 	{
-		// average 16 samples
-		int16_t tmp = 0;
+		// Average 16 samples
+		uint16_t tmp = 0;
 		for (uint8_t i = 0; i < 16; i++) {
 			tmp += tinytouch_adc();
 			_delay_us(100);
 		}
-
-		int32_t delta = bias - tmp;
 		
-		printADCValue(delta);
+		// Subtract bias from measured value
+		uint16_t delta = bias - tmp;
+		serialPrint(delta);
+		
+		// Turn on corresponding leds
 		ledsOff();
 		if (delta > VALUE1) {
 			led5On();
@@ -123,7 +162,7 @@ int main(void)
 	
 	// Initialize touch lib
 	tinytouch_init();
-	bias = 0;
+	bias = 1000;
 	for (uint8_t i = 0; i < 16; i++) {
 		bias += tinytouch_adc();
 		_delay_us(100);
